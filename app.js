63cf5a7621121c8701d7e88cc558dca735f82aac
Shubham Verma <shubhamverma2250@gmail.com>
2024-03-02 12:56:56 +0530

    Sync Async Diff

--- app.js
+++ app.js
@@ -14,21 +14,22 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url == '/message' && method === 'POST') {
-        const body= [];
-        req.on('data',(chunk)=>{
+        const body = [];
+        req.on('data', (chunk) => {
             console.log(chunk);
             body.push(chunk);
         });
 
-        req.on('end',()=>{
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.appendFileSync('message.txt', message);
+            fs.appendFile('message.txt', message, () => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+
         });
-        
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
