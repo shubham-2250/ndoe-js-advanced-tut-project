353395fe10371e9f349f93641a8a2ea128cfb4b4
Shubham Verma <shubhamverma2250@gmail.com>
2024-03-02 12:21:04 +0530

    Send POST through forms

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if (url == '/') {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url == '/message' && method === 'POST') {
+        fs.appendFileSync('message.txt', 'Dummy \n');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node JS Server!</h1></body>');
-    res.write('<html>');
     res.write('</html>');
     res.end();
 });
