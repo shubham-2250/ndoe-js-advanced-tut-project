b5cb53cfdc376c80c041c1debdff72b5dc34f829
Shubham Verma <shubhamverma2250@gmail.com>
2024-03-05 10:34:05 +0530

    Express Router : Use Filtering

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/admin/add-product", (req, res, next) => {
+router.use("/add-product", (req, res, next) => {
   console.log("In the middleware");
   res.send(
     '<form action="/add-product" method ="POST"><input type = "text" name ="title"><button type="submit">Add Product</button></fomr>'
   );
 });
 
-router.post("/admin/add-product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
