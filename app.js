9af8d9ca5bc89d4c7f4f763f607f6fdc0edcc6df
Shubham Verma <shubhamverma2250@gmail.com>
2024-03-06 13:52:49 +0530

    Intro to EJS

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const rootDir = require("./utils/path");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
